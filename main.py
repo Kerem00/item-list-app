27c073cc55d23343a319d719d880ca14369ce00c
Kerem <52210913+Kerem00@users.noreply.github.com>
2021-11-02 08:15:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-#   Item List, lists anything using a name and a comment section.
+#   Item List - Lists anything using a name and a comment section.
 #   Copyright (C) 2021  Kerem Biçen
 
-#   Item List is free software: you can redistribute it and/or modify
+#   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
 
-#   Item List is distributed in the hope that it will be useful,
+#   This program is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #   GNU General Public License for more details.
 
 #   You should have received a copy of the GNU General Public License
-#   along with Item List.  If not, see <https://www.gnu.org/licenses/>.
+#   along with This program.  If not, see <https://www.gnu.org/licenses/>.
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from DialogAdd import Ui_Dialog as FormAdd
