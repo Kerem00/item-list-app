6d45af97ba481d3ee6dfd1164104ccda9f3ba193
Kerem <52210913+Kerem00@users.noreply.github.com>
2021-11-02 08:15:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@
 #   GNU General Public License for more details.
 
 #   You should have received a copy of the GNU General Public License
-#   along with This program.  If not, see <https://www.gnu.org/licenses/>.
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from DialogAdd import Ui_Dialog as FormAdd
